e9e02a909b1b6edc873a941203accd0f6703dc41
ikuma <chemicalbondo@gmail.com>
2022-09-09 10:02:39 +0900

    実行後ディレクトリを開くように

--- create_udemy_coupen_csv.py
+++ create_udemy_coupen_csv.py
@@ -4,7 +4,9 @@ import string
 
 import pandas as pd
 
-coupen_file = "csv_files/coupen_code.csv"
+import open_dir
+
+coupen_file = "coupen_code.csv"
 past_codes_file = "csv_files/past_codes.csv"
 
 coupen_code_len = 10
@@ -19,20 +21,25 @@ def create_random_code(length: int) -> str:
     while randdom_letters in past_codes_list:
         randdom_letters = ''.join(random.choices(letters, k=length))
 
-    with open(past_codes_file, "a") as file:
-        file.writelines(f"{randdom_letters}\n")
-
     return randdom_letters
 
 
-coupon_list = pd.read_csv(coupen_file)
-with open(coupen_file, "w") as new_file:
-    new_file.write(",".join(coupon_list.columns.values) + "\n")
-    for course in coupon_list.values:
-        issuing_date = datetime.datetime.strptime(course[3], "%Y-%m-%d") + datetime.timedelta(days=30)
-        new_file.write(f"{course[0]},"
-                       f"{course[1]},"
-                       f"{create_random_code(coupen_code_len)},"
-                       f"{issuing_date},"
-                       f"{course[4]},"
-                       f"{course[5]}" + "\n")
+if __name__ == "main":
+    coupon_list = pd.read_csv(coupen_file)
+    with open(coupen_file, "w") as new_file:
+        new_file.write(",".join(coupon_list.columns.values) + "\n")
+        for course in coupon_list.values:
+            issuing_date = datetime.datetime.strptime(course[3], "%Y-%m-%d") + datetime.timedelta(days=30)
+            new_code = create_random_code(coupen_code_len)
+
+            new_file.write(f"{course[0]},"
+                           f"{course[1]},"
+                           f"{new_code},"
+                           f"{issuing_date},"
+                           f"{course[4]},"
+                           f"{course[5]}" + "\n")
+
+        with open(past_codes_file, "a") as file:
+            file.writelines(f"{new_code}\n")
+
+    open_dir.open_csv_dir()
