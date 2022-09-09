0634cbb7cf98016271a011ce443e3e59145716b9
ikuma <chemicalbondo@gmail.com>
2022-09-10 08:41:13 +0900

    モジュール名の変更と実行環境による処理分岐

--- open_current_dir.py
+++ open_current_dir.py
@@ -1,8 +1,12 @@
-import webbrowser
+import platform
+import subprocess
 from pathlib import Path
 
 dir_path = Path(__file__).resolve().parent
 
 
-def open():
-    webbrowser.open(f"{dir_path}")
+def open_dir():
+    if platform.system() == "Windows":
+        subprocess.Popen(["explorer", f"{dir_path}"])
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", f"{dir_path}"])
