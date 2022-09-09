396e1dd856eda223844413deaaff1682323d0f07
ikuma <chemicalbondo@gmail.com>
2022-09-09 12:01:08 +0900

    微修正

--- create_udemy_coupen_csv.py
+++ create_udemy_coupen_csv.py
@@ -10,16 +10,17 @@ coupen_file = "coupen_code.csv"
 past_codes_file = "csv_files/past_codes.csv"
 
 coupen_code_len = 10
-letters = string.ascii_uppercase + string.digits + '.' + '_' + '-'
+coupen_lifetime = 30
+effective_letters = string.ascii_uppercase + string.digits + '.' + '_' + '-'
 
 
-def create_random_code(length: int) -> str:
-    randdom_letters = ''.join(random.choices(letters, k=length))
+def create_random_code() -> str:
     with open(past_codes_file, "r") as read_file:
-        past_codes_list = [past_code.replace("\n", "") for past_code in read_file.readlines()]
+        past_codes_list: list[str] = [past_code.replace("\n", "") for past_code in read_file.readlines()]
 
-    while randdom_letters in past_codes_list:
-        randdom_letters = ''.join(random.choices(letters, k=length))
+    randdom_letters: str = ""
+    while randdom_letters == "" or randdom_letters in past_codes_list:
+        randdom_letters = ''.join(random.choices(effective_letters, k=coupen_code_len))
 
     return randdom_letters
 
@@ -29,8 +30,9 @@ if __name__ == "main":
     with open(coupen_file, "w") as new_file:
         new_file.write(",".join(coupon_list.columns.values) + "\n")
         for course in coupon_list.values:
-            issuing_date = datetime.datetime.strptime(course[3], "%Y-%m-%d") + datetime.timedelta(days=30)
-            new_code = create_random_code(coupen_code_len)
+            issuing_date = datetime.datetime.strptime(course[3], "%Y-%m-%d") \
+                           + datetime.timedelta(days=coupen_lifetime)
+            new_code = create_random_code()
 
             new_file.write(f"{course[0]},"
                            f"{course[1]},"
