9982ebb76746d954976a698a87d00e05906eb331
ikuma <chemicalbondo@gmail.com>
2022-09-09 17:11:05 +0900

    変数名と並び順の変更

--- create_udemy_coupon_csv.py
+++ create_udemy_coupon_csv.py
@@ -1,7 +1,7 @@
-import datetime
+import datetime as dt
 import re
 
-import open_dir
+import open_current_dir
 
 origin_coupen_file = "csv_files/udemy_coupon_code.csv"
 coupen_file = "coupon_code.csv"
@@ -18,19 +18,20 @@ if __name__ == "__main__":
             new_file.write(header)
             for line in coupon_lines:
                 cells = line.split(",")
-                issuing_date: datetime = \
-                    datetime.datetime.strptime(cells[3], "%Y-%m-%d") \
-                    + datetime.timedelta(days=coupen_lifespan * issue_times)
-                past_code: str = cells[2]
-                new_issue_times: int = int(re.search(r"\d{4}$", past_code).group()) + issue_times
+
                 course_id: str = cells[0]
-                new_code: str = f"{course_id}-{new_issue_times:04}"
+                current_coupon_code: str = cells[2]
+                new_issue_times: int = int(re.search(r"\d{4}$", current_coupon_code).group()) + issue_times
+                new_coupon_code: str = f"{course_id}-{new_issue_times:04}"
+
+                current_issue_date: dt = dt.datetime.strptime(cells[3], "%Y-%m-%d")
+                new_issue_date: dt = current_issue_date + dt.timedelta(days=coupen_lifespan * issue_times)
 
                 new_file.write(f"{course_id},"
                                f"{cells[1]},"
-                               f"{new_code},"
-                               f"{issuing_date},"
+                               f"{new_coupon_code},"
+                               f"{new_issue_date},"
                                f"{cells[4]},"
                                f"{cells[5]}")
 
-        open_dir.open_csv_dir()
+        open_current_dir.open()
