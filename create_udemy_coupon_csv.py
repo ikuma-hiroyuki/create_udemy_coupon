586749556164966790f10253b710af0181119ece
ikuma <chemicalbondo@gmail.com>
2022-09-09 17:00:35 +0900

    標準ライブラリのみで実装するように

--- create_udemy_coupon_csv.py
+++ create_udemy_coupon_csv.py
@@ -1,8 +1,6 @@
 import datetime
 import re
 
-import pandas as pd
-
 import open_dir
 
 origin_coupen_file = "csv_files/udemy_coupon_code.csv"
@@ -11,22 +9,28 @@ coupen_lifespan = 30
 
 if __name__ == "__main__":
     issue_times: int = int(input("Please enter the number of issues.\n"))
-    coupon_list = pd.read_csv(origin_coupen_file)
-    with open(coupen_file, "w") as new_file:
-        new_file.write(",".join(coupon_list.columns.values) + "\n")
-        for course in coupon_list.values:
-            issuing_date: datetime = datetime.datetime.strptime(course[3], "%Y-%m-%d") \
-                                     + datetime.timedelta(days=coupen_lifespan * issue_times)
-            past_code: str = course[2]
-            new_issue_times: int = int(re.search(r"\d{4}$", past_code).group()) + issue_times
-            course_id: str = course[0]
-            new_code: str = f"{course_id}-{new_issue_times:04}"
+    with open(origin_coupen_file, "r") as origin_file:
+        header: str = origin_file.readline()
+        origin_file.__next__()
+        coupon_lines = origin_file.readlines()
 
-            new_file.write(f"{course_id},"
-                           f"{course[1]},"
-                           f"{new_code},"
-                           f"{issuing_date},"
-                           f"{course[4]},"
-                           f"{course[5]}" + "\n")
+        with open(coupen_file, "w") as new_file:
+            new_file.write(header)
+            for line in coupon_lines:
+                cells = line.split(",")
+                issuing_date: datetime = \
+                    datetime.datetime.strptime(cells[3], "%Y-%m-%d") \
+                    + datetime.timedelta(days=coupen_lifespan * issue_times)
+                past_code: str = cells[2]
+                new_issue_times: int = int(re.search(r"\d{4}$", past_code).group()) + issue_times
+                course_id: str = cells[0]
+                new_code: str = f"{course_id}-{new_issue_times:04}"
 
-    open_dir.open_csv_dir()
+                new_file.write(f"{course_id},"
+                               f"{cells[1]},"
+                               f"{new_code},"
+                               f"{issuing_date},"
+                               f"{cells[4]},"
+                               f"{cells[5]}")
+
+        open_dir.open_csv_dir()
