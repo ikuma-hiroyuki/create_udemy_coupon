ebcaf42435eea7845d6567f744390cdb7d6b7354
ikuma <chemicalbondo@gmail.com>
2022-10-04 12:13:08 +0900

    csvライブラリで読書に変更

--- create_udemy_coupon_csv.py
+++ create_udemy_coupon_csv.py
@@ -1,3 +1,4 @@
+import csv
 import datetime as dt
 
 import coupon_pathlib as cp
@@ -5,32 +6,25 @@ import coupon_pathlib as cp
 COUPEN_LIFESPAN = 30
 
 
-def get_issue_date(csv_line: str) -> dt:
-    line_cells = csv_line.split(",")
-    return dt.datetime.strptime(line_cells[3], "%Y-%m-%d")
+def get_new_items(start_date: dt) -> (str, str):
+    origin_issue_date: dt = dt.datetime.strptime(start_date, "%Y-%m-%d")
+    next_issue_times: int = (dt.datetime.today() - origin_issue_date).days // COUPEN_LIFESPAN + 1
+    next_issue_date: dt = origin_issue_date + dt.timedelta(days=COUPEN_LIFESPAN * next_issue_times)
+    return f"{row['course_id']}-{next_issue_times:04}", next_issue_date.strftime("%Y-%m-%d")
 
 
 if __name__ == "__main__":
-    with open(cp.origin_coupen_file, "r") as origin_file:
-        header: str = origin_file.readline()
-        origin_file.__next__()
-        origin_coupon_lines: list[str] = origin_file.readlines()
-        origin_issue_date: dt = get_issue_date(origin_coupon_lines[0])
-        new_issue_times: int = (dt.datetime.today() - origin_issue_date).days // COUPEN_LIFESPAN + 1
-        new_issue_date: dt = origin_issue_date + dt.timedelta(days=COUPEN_LIFESPAN * new_issue_times)
+    with open(cp.origin_coupen_file, "r", encoding="utf-8", newline="") as origin_file:
+        reader = csv.DictReader(origin_file)
 
-    with open(cp.new_coupon_file, "w") as new_file:
-        new_file.write(header)
-        for line_string in origin_coupon_lines:
-            cells = line_string.split(",")
-            course_id: str = cells[0]
-            new_coupon_code: str = f"{course_id}-{new_issue_times:04}"
+        with open(cp.new_coupon_file, "w", encoding="utf-8", newline="") as new_file:
+            writer = csv.DictWriter(new_file, fieldnames=reader.fieldnames)
+            writer.writeheader()
 
-            new_file.write(f"{course_id},"
-                           f"{cells[1]},"  # coupon_type
-                           f"{new_coupon_code},"
-                           f"{new_issue_date},"
-                           f"{cells[4]},"  # start_time
-                           f"{cells[5]}")  # custom_price
+            for row in reader:
+                new_coupon_code, new_issue_date = get_new_items(row["start_date"])
+                row["start_date"] = new_issue_date
+                row["coupon_code"] = new_coupon_code
+                writer.writerow(row)
 
     cp.open_new_coupon_dir()
