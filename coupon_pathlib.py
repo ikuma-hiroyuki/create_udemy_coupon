e291d87649eebe3de9c372378e09ee8cafc97974
ikuma <chemicalbondo@gmail.com>
2023-07-27 17:33:37 +0900

    新規発行したクーポンコードを取得する関数を追加した

--- coupon_pathlib.py
+++ coupon_pathlib.py
@@ -1,3 +1,4 @@
+import csv
 import platform
 import subprocess
 from pathlib import Path
@@ -14,3 +15,18 @@ def open_new_coupon_dir():
         subprocess.Popen(["explorer", f"{new_coupon_dir}"])
     elif platform.system() == "Darwin":
         subprocess.Popen(["open", f"{new_coupon_dir}"])
+
+
+def get_coupon_dict():
+    """
+    発行したクーポンコードを取得する。
+
+    :return: coupon_dict = {course_id: coupon_code}
+    """
+
+    coupon_dict = {}
+    with new_coupon_file.open("r", encoding="utf-8") as f:
+        reader: csv.DictReader = csv.DictReader(f)
+        for row in reader:
+            coupon_dict[row["course_id"]] = row["coupon_code"]
+    return coupon_dict
