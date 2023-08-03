c386b8a7297e0707a5f518ae1a27ef9912e0b693
ikuma <chemicalbondo@gmail.com>
2023-08-03 15:15:20 +0900

    連番クーポンとユニークなクーポンと分けて作れるようにした

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
+"""
+Udemyのクーポン一括作成機能用のクーポンを作成する
+https://www.udemy.com/instructor/multiple-coupons-creation/
+"""
+
 import csv
-import datetime as dt
+import datetime
 
-import coupon_pathlib as cp
+from utils import coupon_pathlib as cp, create_increment_code, create_unique_id, convert_jst_to_pst
 
-COUPON_LIFESPAN = 30
-ORIGIN_ISSUE_DATE = dt.datetime(2022, 9, 7)  # 一番最初にクーポンを発行した日付
 
-with cp.courses_file.open("r", encoding="utf-8") as f:
-    reader: csv.DictReader = csv.DictReader(f)
-    next_issue_times = (dt.datetime.today() - ORIGIN_ISSUE_DATE).days // COUPON_LIFESPAN + 1
-    start_date = (ORIGIN_ISSUE_DATE + dt.timedelta(days=COUPON_LIFESPAN * next_issue_times)).strftime("%Y-%m-%d")
+def create_udemy_coupon(coupon_type: str, start_date: str, start_time: str):
+    """
+    Udemyのクーポン一括作成機能用のクーポンを作成する
 
-    new_coupon_list = []
-    for course in reader:
-        new_coupon_list.append(
-            {
-                "course_id": course["course_id"],
-                "coupon_type": "custom_price",
-                "coupon_code": f"{course['course_id']}-{next_issue_times:04}",
-                "start_date": start_date,
-                "start_time": "0:00",
-                "custom_price": "min"
-            }
-        )
+    coupon_typeがcustom_priceの場合は、コースID+連番でクーポンコードを作成する
+    :param coupon_type:
+    :param start_date:
+    :param start_time:
+    :return:
+    """
 
-with cp.new_coupon_file.open("w", encoding="utf-8", newline="") as f2:
-    fieldnames = new_coupon_list[0].keys()
-    writer = csv.DictWriter(f2, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(new_coupon_list)
+    # クーポンのタイプによって、クーポンコードを作成する関数を変える
+    coupon_func = None
+    is_custom_price = coupon_type == "custom_price"
+    if is_custom_price:
+        coupon_func = create_increment_code
+    else:
+        coupon_func = create_unique_id
 
-cp.open_new_coupon_dir()
+    # 対象になるコースに対して、クーポンコードを作成する
+    with cp.courses_file.open("r", encoding="utf-8") as f:
+        reader: csv.DictReader = csv.DictReader(f)
+
+        new_coupon_list = []
+        for course in reader:
+            if is_custom_price:
+                coupon_code = coupon_func(course["course_id"])
+            else:
+                coupon_code = coupon_func()
+
+            new_coupon_list.append(
+                {
+                    "course_id": course["course_id"],
+                    "coupon_type": coupon_type,
+                    "coupon_code": coupon_code,
+                    "start_date": start_date,
+                    "start_time": start_time,
+                    "custom_price": "min" if is_custom_price else None,
+                }
+            )
+
+    with cp.new_coupon_file.open("w", encoding="utf-8", newline="") as f2:
+        fieldnames = new_coupon_list[0].keys()
+        writer = csv.DictWriter(f2, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(new_coupon_list)
+
+    cp.open_new_coupon_dir()
+
+
+jp_start_date, jp_start_time = convert_jst_to_pst(datetime.datetime.now())
+create_udemy_coupon("best_price", jp_start_date, jp_start_time)
