768a39b191eee029e4d0dff03fe1e9d532169f8c
ikuma-hiroyuki <chemicalbondo@gmail.com>
2023-08-25 19:41:06 +0900

    コマンドライン引数のbest, customオプションを変更した

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import datetime
 from utils import coupon_pathlib as cp, create_increment_code, create_unique_id, convert_jst_to_pst
 
 
-def create_udemy_coupon(is_custom_price: bool, start_date: str, start_time: str):
+def create_udemy_coupon(is_best_price: bool, start_date: str, start_time: str):
     """
     Udemyのクーポン一括作成機能用のクーポンを作成する
 
     coupon_typeがcustom_priceの場合は、コースID+連番でクーポンコードを作成する
-    :param is_custom_price: custom_priceにするかどうか。Trueの場合はcustom_priceにし、Falseの場合はbest_priceにする
+    :param is_best_price: best_priceにするかどうか。Trueの場合はbest_priceにし、Falseの場合はcustom_priceにする
     :param start_date: 開始日(日本時間)
     :param start_time: 開始時刻(日本時間)
     """
@@ -23,12 +23,12 @@ def create_udemy_coupon(is_custom_price: bool, start_date: str, start_time: str)
     jp_start_date, jp_start_time = convert_jst_to_pst(start_date_time)
 
     # クーポンのタイプによって、クーポンコードを作成する関数を変える
-    if is_custom_price:
-        coupon_type = "custom_price"
-        coupon_func = create_increment_code
-    else:
+    if is_best_price:
         coupon_type = "best_price"
         coupon_func = create_unique_id
+    else:
+        coupon_type = "custom_price"
+        coupon_func = create_increment_code
 
     # 対象になるコースに対して、クーポンコードを作成する
     with cp.courses_file.open("r", encoding="utf-8") as f:
@@ -45,7 +45,7 @@ def create_udemy_coupon(is_custom_price: bool, start_date: str, start_time: str)
                     "coupon_code": coupon_code,
                     "start_date": jp_start_date,
                     "start_time": jp_start_time,
-                    "custom_price": "min" if is_custom_price else None,
+                    "custom_price": "min" if is_best_price else None,
                 }
             )
 
@@ -60,15 +60,15 @@ def create_udemy_coupon(is_custom_price: bool, start_date: str, start_time: str)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Udemyのクーポン一括作成機能用のクーポンを作成する")
-    parser.add_argument("-c", "--is_custom_price", action="store_true", help="custom_priceのクーポンを作成するかどうか")
+    parser.add_argument("-b", "--is_best_price", action="store_true", help="best_priceのクーポンを作成するかどうか")
     parser.add_argument('-d', '--start_date', type=str, help='YYYY-MM-DD (省略すると当日)')
     parser.add_argument('-t', '--start_time', type=str, help='HH:MM (省略すると現在時刻)')
     args = parser.parse_args()
 
-    if args.is_custom_price:
-        price_type = "Custom price"
-    else:
+    if args.is_best_price:
         price_type = "Best price"
+    else:
+        price_type = "Custom price"
 
     if args.start_date is None:
         args.start_date = datetime.date.today().strftime("%Y-%m-%d")
@@ -77,4 +77,4 @@ if __name__ == '__main__':
 
     is_create = input(f"{price_type} でクーポンを作成しますか？(y/n): ")
     if is_create == "y":
-        create_udemy_coupon(args.is_custom_price, args.start_date, args.start_time)
+        create_udemy_coupon(args.is_best_price, args.start_date, args.start_time)
